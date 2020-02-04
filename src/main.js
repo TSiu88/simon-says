7e290fc2e38064c227684a05f9be0e87a4cbb86b
Student <example@example.com>
2020-02-03 16:48:23 -0800

    add js and html

--- src/main.js
+++ src/main.js
@@ -1,3 +1,61 @@
 // User Interface logic
 // use "import {Object} from ./businesslogicfilename';"
 import './styles.css';
+import {randomColor} from './simon';
+import {flashColor} from './simon';
+
+$(document).ready(function(){
+  
+  $("#startButton").click(function(event){
+    event.preventDefault();
+
+    $("#gameOutput").append(randomColor());
+    flashColor();
+    // then change display
+
+  });
+
+  var playerArray = [];
+
+  $("#blueButton").click(function(event){
+    event.preventDefault();
+    playerArray.push("blue");
+    console.log(playerArray);
+    $("#playerOutput").text(playerArray);
+  });
+
+  $("#redButton").click(function(event){
+    event.preventDefault();
+    playerArray.push("red");
+    console.log(playerArray);
+    $("#playerOutput").text(playerArray);
+  });
+
+  $("#yellowButton").click(function(event){
+    event.preventDefault();
+    playerArray.push("yellow");
+    console.log(playerArray);
+    $("#playerOutput").text(playerArray);
+  });
+
+  $("#greenButton").click(function(event){
+    event.preventDefault();
+    playerArray.push("green");
+    console.log(playerArray);
+    $("#playerOutput").text(playerArray);
+  });
+
+  $("#submitButton").click(function(event){
+    event.preventDefault();
+    // take playerArray and compare to colorArray
+    // if correct, continue game
+    // else game over
+    // reset playerArray
+  });
+
+  $("#playAgain").click(function(event){
+    event.preventDefault();
+    //reset color array and player array
+  });
+
+});
--- src/simon.js
+++ src/simon.js
@@ -1,2 +1,28 @@
 // Business end logic
 //use "export function Object(parameter)"
+var colorArray = [];
+
+export function randomColor() {
+  var colors = ["blue", "red", "yellow", "green"];
+  var randomNum = Math.floor(Math.random()* colors.length);
+  console.log(randomNum);
+  colorArray.push(colors[randomNum]);
+  console.log(colorArray);
+  return colors[randomNum];
+
+}
+
+export function flashColor(){
+  for (var i = 0; i < colorArray.length; i++){
+    var currColor = colorArray[i];
+    setInterval(showColor(currColor), 2000);
+  }
+}
+
+function showColor(color){
+  if(color === "blue"){
+    //show picture of blue color?
+    return "blue";
+  }
+  //do for rest of colors
+}
